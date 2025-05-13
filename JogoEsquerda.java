41bce47bc51595d5790c7ca406bc2517ef3b92b1
marcos <206247214+MarcosGitCode@users.noreply.github.com>
2025-05-13 20:43:41 -0300

    botao porta

--- JogoEsquerda.java
+++ JogoEsquerda.java
@@ -4,18 +4,18 @@ import javax.swing.*;
 public class JogoEsquerda extends BasePainelComBotao {
 
     public JogoEsquerda(CardLayout layout, JPanel painelPrincipal) {
-        super("imagens/Fotos editadas/Botoeiras e sinaleira lateral - detalhe.jpg", layout, painelPrincipal);
+        super("imagens/Fotos editadas/06 - Botoeiras e sinaleira lateral.jpg", layout, painelPrincipal);
         setLayout(null); // Layout absoluto para posicionar componentes, se necessário
 
         painelPrincipal.add(new Trem1(layout, painelPrincipal), "Trem1");
 
         JPanel painelBotoes = new JPanel();
-        painelBotoes.setBounds(0, 0, 800, 600);
+        painelBotoes.setBounds(0, 0, 856, 800);
         painelBotoes.setLayout(null);
         painelBotoes.setOpaque(false); // Torna o painel transparente
         add(painelBotoes);
 
-        criarBotaoVisivel(painelBotoes, 10, 200, 200, 200, Color.GREEN, e -> {
+        criarBotaoVisivel(painelBotoes, 0, 0, 500, 800, Color.GREEN, e -> {
             System.out.println("Botão Trem1 clicado!");
             layout.show(painelPrincipal, "Trem1"); // Alterna para o painel JogoTrem1
         });
@@ -24,9 +24,8 @@ public class JogoEsquerda extends BasePainelComBotao {
     private void criarBotaoVisivel(JPanel painel, int x, int y, int width, int height, Color color, java.awt.event.ActionListener action) {
         JButton botao = new JButton();
         botao.setBounds(x, y, width, height);
-        botao.setBackground(color); // Define a cor de fundo como verde
-        botao.setContentAreaFilled(false); // Remove o fundo visível
-         // Garante que o botão seja transparente
+        botao.setOpaque(false); // Torna o botão transparente
+        botao.setContentAreaFilled(false); // Remove a área de fundo
         botao.setBorderPainted(false); // Remove as bordas do botão
         botao.addActionListener(action);
         painel.add(botao);
